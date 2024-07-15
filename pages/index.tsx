24f6623026ac1081837bbd8d937dba176b7e90da
BUK Dev <buktoken@gmail.com>
2024-07-15 17:28:50 +0100

    full size mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,7 +67,7 @@ const Home: NextPage = () => {
         },
         scale: mobile
           ? {
-              mode: Phaser.Scale.RESIZE,
+              mode: Phaser.Scale.FIT,
               autoCenter: Phaser.Scale.CENTER_BOTH,
               width: '100%',
               height: '100%',
