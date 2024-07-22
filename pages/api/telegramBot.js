ce403b2143521fe40ff019eb3987344b7ba023a8
BUK Dev <buktoken@gmail.com>
2024-07-22 20:27:25 +0100

    updated hook

--- pages/api/telegramBot.js
+++ pages/api/telegramBot.js
@@ -11,10 +11,15 @@ bot.start((ctx) => {
 
 bot.launch();
 
+// Set up a webhook to keep the bot running
+app.use(bot.webhookCallback('/secret-hook'));
+
+bot.telegram.setWebhook('https://buk-bash-git-mobile-buks-projects-c5fbd1d8.vercel.app/secret-hook');
+
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
-app.listen(3001, () => {
-  console.log('Bot server running on port 3001');
+app.listen(3000, () => {
+  console.log('Bot server running on port 3000');
 });
