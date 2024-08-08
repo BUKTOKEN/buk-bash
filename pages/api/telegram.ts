889e016bcbacd71aa1af87c397314484d0452827
BUK Dev <buktoken@gmail.com>
2024-08-08 21:03:34 +0100

    remove type

--- pages/api/telegram.ts
+++ pages/api/telegram.ts
@@ -25,7 +25,7 @@ bot.launch();
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req);
-  if (req.method === 'POST') {
+  // if (req.method === 'POST') {
     try {
       bot.handleUpdate(req.body);
       res.status(200).json({ status: 'ok' });
@@ -33,9 +33,9 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
       console.error('Error handling update:', error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
-  } else {
-    res.status(404).json({ error: 'Not Found' });
-  }
+//  } else {
+ //   res.status(404).json({ error: 'Not Found' });
+  // }
 }
 
 // Enable graceful stop
