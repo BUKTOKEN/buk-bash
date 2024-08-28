ead944b412d704e7379aa8d8fef7e8fefff3d9b1
darylclarke <dclarke.developer@gmail.com>
2024-08-28 19:15:48 +0100

    Removing center

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,7 +60,7 @@ const Home: NextPage = () => {
           scale: mobile
             ? {
               mode: Phaser.Scale.FIT,
-              autoCenter: Phaser.Scale.CENTER_BOTH,
+              autoCenter: Phaser.Scale.NO_CENTER,
               width: '100%',
               height: '100%',
             }
