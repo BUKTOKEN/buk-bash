d77edad538be19992a558d50ed72e6275ca38049
darylclarke <dclarke.developer@gmail.com>
2024-09-14 13:40:37 +0100

    lazy load script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import type { AppProps } from "next/app";
+import Script from 'next/script';
 import Head from "next/head";
 import { BaseSepoliaTestnet } from "@thirdweb-dev/chains";
 import "./styles/globals.css";
@@ -11,7 +12,14 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>BUK Bash</title>
         {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
-        <script src="https://telegram.org/js/telegram-web-app.js"></script>
+       
+      <Script
+        src="https://telegram.org/js/telegram-web-app.js"
+        strategy="lazyOnload"
+        onLoad={() => {
+          console.log('Telegram Web App SDK loaded.');
+        }}
+      />
         <meta
           name="description"
           content="BUK Bash - NFT reward"
